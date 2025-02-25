a117cfb9c85482fb8db140044a8a1ec48168dc69
TimurAztec <timotiaztec@gmail.com>
2025-02-26 01:39:44 +0200

    Added protection from texting when user is online

--- __main__.py
+++ __main__.py
@@ -3,20 +3,24 @@ import io
 import os
 import random
 import ffmpeg
+import whisper
 from PIL import Image
 from datetime import datetime, timedelta
 import openai
+import asyncio
 from openai import AsyncOpenAI
 from telethon import TelegramClient, events
 from collections import defaultdict
 from dotenv import load_dotenv
 from telethon.tl.functions.account import GetAuthorizationsRequest
+from telethon.tl.functions.messages import SetTypingRequest
+from telethon.tl.types import SendMessageTypingAction
 
 load_dotenv()
 API_ID = os.getenv("TELEGRAM_API_ID")
 API_HASH = os.getenv("TELEGRAM_API_HASH")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ALLOWED_GROUP_IDS = os.getenv("GROUP_WHITE_LIST").split(',')
+CHAT_WHITE_LIST = os.getenv("CHAT_WHITE_LIST").split(',')
 SYS_PROMPT = os.getenv("SYS_PROMPT")
 
 client = TelegramClient("session", int(API_ID), API_HASH)
@@ -25,9 +29,11 @@ openai_client = AsyncOpenAI(
 )
 
 reply_enabled = True
+busy_replying = defaultdict(lambda: False)
 chats_history = defaultdict(list)
 
 NUM_PREVIOUS_MESSAGES = 11
+TYPING_SPEED = 11
 
 @client.on(events.NewMessage(incoming=False))
 async def toggle_reply(event):
@@ -41,17 +47,24 @@ async def toggle_reply(event):
         elif event.text.lower() == "reply-off" and reply_enabled:
             reply_enabled = False
             await event.respond("❌ Auto-reply is OFF.")
+    elif event.chat_id != me.id:
+        if event.text.lower() == "reply-add" and not str(event.chat_id) in CHAT_WHITE_LIST:
+            CHAT_WHITE_LIST.append(str(event.chat_id))
+            print(f"Chat added: {event.chat_id}")
+        elif event.text.lower() == "reply-remove" and str(event.chat_id) in CHAT_WHITE_LIST:
+            CHAT_WHITE_LIST.remove(str(event.chat_id))
+            print(f"Chat removed: {event.chat_id}")
 
 @client.on(events.NewMessage(incoming=True))
 async def handle_private_message(event):
-    global reply_enabled
+    global reply_enabled, busy_replying
     me = await client.get_me()
     print(f"Incoming | Chat id: {event.chat_id} | Text: {event.text}")
     
-    if (event.is_group and str(event.chat_id) not in ALLOWED_GROUP_IDS) or event.chat_id == me.id or (event.text == '' and not (event.photo or event.document)):
+    if str(event.chat_id) not in CHAT_WHITE_LIST or event.chat_id == me.id or (event.text == '' and not (event.photo or event.document or event.voice)):
         return
     
-    sender_id = event.sender_id if not event.is_group else event.chat_id
+    sender_id = event.chat_id if event.is_group else event.sender_id
 
     if not chats_history[sender_id]:
         previous_messages = await client.get_messages(event.chat_id, limit=NUM_PREVIOUS_MESSAGES)
@@ -61,15 +74,15 @@ async def handle_private_message(event):
                 chats_history[sender_id].append({"role": "user", "content": msg.text})
             if msg.from_id == me.id:
                 chats_history[sender_id].append({"role": "assistant", "content": msg.text})
-
-    chats_history[sender_id].append({"role": "user", "content": event.text})
     
-    if event.is_group and not check_mention(me, sender_id, event):
+    if (event.is_group and not check_mention(me, sender_id, event)) or busy_replying[sender_id]:
         return
 
-    if not reply_enabled or not await check_active_sessions():
+    active = await check_active_sessions()
+    if not reply_enabled or active:
         return
 
+    busy_replying[sender_id] = True
     try:
         system_message = {
             "role": "system",
@@ -78,8 +91,12 @@ async def handle_private_message(event):
         history = chats_history[sender_id][-NUM_PREVIOUS_MESSAGES:]
         history.insert(0, system_message)
 
+        if event.text:
+            history.append({"role": "user", "content": event.text})
+
+        image_base64 = None
         if event.photo or event.document:
-            print(f"Image type: {event.document.mime_type}")
+            print(f"Document type: {event.document.mime_type}")
             if event.document and event.document.mime_type == "image/gif":
                 blob = await event.download_media(bytes)
                 image = Image.open(io.BytesIO(blob))
@@ -112,40 +129,67 @@ async def handle_private_message(event):
                 buffer = io.BytesIO()
                 image.save(buffer, format="JPEG")
                 image_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+            elif event.document and event.document.mime_type == "audio/ogg":
+                audio_file = await event.download_media()
+
+                model = whisper.load_model("base")
+                transcribed_text = model.transcribe(audio_file)
+                print(f"Audio transcription: {transcribed_text.get('text')}")
+                history.append({"role": "user", "content": transcribed_text.get('text')})
+                os.remove(audio_file)
             else:
                 blob = await event.download_media(bytes)
                 image_base64 = base64.b64encode(blob).decode("utf-8")
 
-            history.append({
-                "role": "user",
-                "content": [
-                    {"type": "image_url",
-                     "image_url": {"url": f"data:image/jpeg;base64,{image_base64}", "detail": "auto"}}
-                ]
-            })
+            if image_base64:
+                history.append({
+                    "role": "user",
+                    "content": [
+                        {"type": "image_url",
+                         "image_url": {"url": f"data:image/jpeg;base64,{image_base64}", "detail": "auto"}}
+                    ]
+                })
 
         response = await openai_client.chat.completions.create(
-            model="gpt-4-turbo",
+            model="gpt-4",
             messages=history,
-            max_tokens=round(random.uniform(333, 1000)),
-            temperature=random.uniform(0.25, 0.666)
+            max_tokens=round(random.uniform(100, 333)),
+            temperature=random.uniform(0.5, 1),
+            presence_penalty=-1.5,
+            frequency_penalty=0.0
         )
         
         explanation = response.choices[0].message.content.strip()
+        # print(response.choices[0])
+        # stop_flag = response.choices[0].get("stop_conversation", False)
 
-        if not explanation or explanation[-1] not in ".!?":
-            explanation += "."
+        # if stop_flag:
+        #     raise ValueError(f"Conversation with {sender_id} is over.")
 
-        await event.respond(explanation)
+        await simulate_typing(event, explanation)
+        await event.reply(explanation) if event.is_group else await event.respond(explanation)
 
         chats_history[sender_id].append({"role": "assistant", "content": explanation})
 
     except openai._exceptions.RateLimitError:
         print("Quota limit exceeded or rate limit error")
-        return ""
+        return
     except Exception as e:
         print(f"An error occurred: {e}")
-        return ""
+        return
+    finally:
+        busy_replying[sender_id] = False
+
+async def simulate_typing(event, text):
+    chat_id = event.chat_id
+    try:
+        await asyncio.sleep(round(random.uniform(0.5, 5)))
+        await client(SetTypingRequest(chat_id, SendMessageTypingAction()))
+        typing_time = round(len(text) / TYPING_SPEED)
+        print(f"Typing for: {typing_time}")
+        await asyncio.sleep(typing_time)
+    except Exception as e:
+        print(f"Error while sending typing action: {e}")
     
 def check_mention(me, sender_id, event):
     mention = event.is_reply or (f"@{me.username}" in event.text) or chats_history[sender_id][-2].get("role") == "assistant"
@@ -153,13 +197,16 @@ def check_mention(me, sender_id, event):
 
 async def check_active_sessions():
     global client
+    return False
     auths = await client(GetAuthorizationsRequest())
 
     now = datetime.utcnow().replace(tzinfo=None)
-    active_threshold = timedelta(minutes=1)
+    print(f"Session time now: {now}")
+    active_threshold = timedelta(minutes=3)
 
     for session in auths.authorizations:
         last_active = session.date_active.replace(tzinfo=None)
+        print(f"Session time last active: {last_active}")
 
         if now - last_active < active_threshold:
             print("User is online - no response")
